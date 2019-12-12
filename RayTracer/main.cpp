62aa9d066ee849f226d145cfa1db9bd2f1a4842c
Travis James Smith <trajamsmith@gmail.com>
2019-12-12 18:37:32 -0500

    Initial Commit

---
+++ RayTracer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RayTracer
+//
+//  Created by Beeb on 12/12/19.
+//  Copyright © 2019 Beeb. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
